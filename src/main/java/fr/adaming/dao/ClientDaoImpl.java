7efc65323d1e35be7943cad7c32366a54c9aaa6d
spnk <justine.gmrd@gmail.com>
2020-06-04 20:41:00 +0200

    dao commande

--- src/main/java/fr/adaming/dao/ClientDaoImpl.java
+++ src/main/java/fr/adaming/dao/ClientDaoImpl.java
@@ -1,8 +1,31 @@
 package fr.adaming.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
+
+import fr.adaming.entities.Client;
+import fr.adaming.entities.Commande;
 
 @Repository
 public class ClientDaoImpl implements IClientDao {
+
+	private SessionFactory sf;
+	
+	@Override
+	public Client registerClient(Client clientIn, Commande commandeIn) {
+		
+		Session s = sf.getCurrentSession();
+		
+		clientIn.getListeCommandes().add(commandeIn);
+		s.save(clientIn);
+		 
+		 return clientIn;
+		
+		
+	}
+	
+	
+	
 
 }
